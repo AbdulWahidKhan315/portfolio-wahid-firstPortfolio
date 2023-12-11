ccae944a85c5cbed71f7c8c813091d554165d62d
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-12-12 01:14:09 +0600

    Completed the About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,82 @@
 import React from 'react';
 
+import CountUp from 'react-countup';
+
+import { useInView } from 'react-intersection-observer'
+
+import { motion } from 'framer-motion'
+
+import { fadeIn } from '../variants'
+
 const About = () => {
-  return <div className='section' id='about'>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+          {/* img */}
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'></motion.div>
+          {/* text */}
+          <motion.div
+            variants={fadeIn('left', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1'>
+            <h2 className='h2 text-accent'>About me.</h2>
+            <h3 className='h3 mb-4'>
+              I'm a React Front-end Web Developer. I always focus on making error free website and also on user satisfaction.
+            </h3>
+            <p className='mb-6'>
+              I'm a React Front-end Web Developer. I always focus on making error free website and also on user satisfaction.I'm a React Front-end Web Developer. I always focus on making
+            </p>
+            {/* stats */}
+            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {inView ? <CountUp start={0} end={12} duration={3}></CountUp> :
+                    null}
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Yers of <br />
+                  Experience
+                </div>
+              </div>
+
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {inView ? <CountUp start={0} end={15} duration={3}></CountUp> :
+                    null}K+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Project <br />
+                  Completed
+                </div>
+              </div>
+
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {inView ? <CountUp start={0} end={8} duration={3}></CountUp> :
+                    null}K+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Satisfied <br />
+                  Client
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default About;
